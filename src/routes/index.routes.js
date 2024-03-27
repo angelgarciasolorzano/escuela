78fa0cd05da542c4351be6e26c805b6b16cd6b52
angelgarciasolorzano <156849177+angelgarciasolorzano@users.noreply.github.com>
2024-03-26 21:00:41 -0600

    creando nuevas funciones

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('inicio');
+  res.render('inicio', { styles: '<link rel="stylesheet" href="/css/inicio.css">' });
 });
 
 export default router;
