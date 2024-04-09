dd382e18bc5608fd62e6068f0484cf05b61cd32f
angelgarciasolorzano <156849177+angelgarciasolorzano@users.noreply.github.com>
2024-04-09 05:31:48 -0600

    agregando mensajes-error

--- src/public/event/addestudiantes.js
+++ src/public/event/addestudiantes.js
@@ -3,27 +3,28 @@ const apellido = document.getElementById('add-apellidos');
 const direccion = document.getElementById('add-direccion');
 const edad = document.getElementById('add-edad');
 const form = document.querySelector('form');
-const botonEstudiantes = document.querySelector('.contenedor-todo');
+const mensaje = document.querySelector('.contenedor-alerta');
 
 function showError(message) {
-  if (document.querySelector('.mensaje')) { return; }
+  const mensajeAnterior = document.querySelector('.mensaje');
+  if (mensajeAnterior) {
+    mensajeAnterior.remove();
+  }
 
   const errorDiv = document.createElement('div');
   errorDiv.className = 'mensaje';
   errorDiv.innerHTML = `
-    <div class="error">
-      <div class="error__icon">
-        <i class="bi bi-exclamation-circle-fill"></i>
-      </div>
-      <div class="error__title">${message}</div>
-      <div class="error__close">
-        <i class="bi bi-x-lg close-btn"></i>
-      </div>
-    </div>`;
-  botonEstudiantes.parentNode.insertBefore(errorDiv, botonEstudiantes.nextSibling);
-
-  const closeBtn = document.querySelector('.close-btn');
-  closeBtn.addEventListener('click', () => { errorDiv.remove(); });
+  <div class="container p-2">
+    <div class="row">
+      <div class="col-md-5 mx-auto">
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          <strong>Advertencia!</strong> ${ message }.
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      </di>
+    </div>
+  </div>`;
+  mensaje.parentNode.insertBefore(errorDiv, mensaje);
 };
 
 form.addEventListener('submit', (e) => {
--- src/public/event/loginform.js
+++ src/public/event/loginform.js
@@ -1,59 +1,30 @@
-const optionMenu = document.querySelector(".select-menu"), selectBtn = optionMenu.querySelector(".select-btn"),
-options = optionMenu.querySelectorAll(".option"), 
-text = optionMenu.querySelector(".sBtn-text");
-
 const form = document.querySelector('form');
 const emailInput = document.getElementById('login-usuario');
 const passwordInput = document.getElementById('login-pass');
 const cargoInput = document.getElementById('login-cargo');
-const submitBtn = document.querySelector('.sesion');
-
-selectBtn.addEventListener('click', () => optionMenu.classList.toggle("active"));
-
-options.forEach(option => {
-  option.addEventListener('click', () => {
-    let selectedOption = option.querySelector(".option-text").innerText;
-
-    text.value = selectedOption;
-    console.log(selectedOption);
-    optionMenu.classList.add("active")
-  });
-});
-
-const showHiddenPass = (loginPass, loginEye) => {
-  const input = document.getElementById(loginPass), iconEye = document.getElementById(loginEye);
-
-  iconEye.addEventListener('click', () => {
-    if (input.type === 'password') {
-      input.type = 'text';
-      iconEye.classList.add('ri-eye-line');
-      iconEye.classList.remove('ri-eye-off-line');
-    } else {
-      input.type = 'password';
-      iconEye.classList.remove('ri-eye-line');
-      iconEye.classList.add('ri-eye-off-line');
-    }
-  });
-};
-showHiddenPass('login-pass', 'login-eye');
 
 function showError(message) {
-  if (document.querySelector('.error')) { return; }
+  const mensajeAnterior = document.querySelector('.error');
+  if (mensajeAnterior) {
+    mensajeAnterior.remove();
+  }
 
   const errorDiv = document.createElement('div');
   errorDiv.className = 'error';
   errorDiv.innerHTML = `
-    <div class="error__icon">
-      <i class="bi bi-exclamation-circle-fill"></i>
+  <div class="container p-2">
+    <div class="row">
+      <div class="col-md-5 mx-auto">
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          <strong>Advertencia!</strong> ${ message }.
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      </di>
     </div>
-    <div class="error__title">${message}</div>
-    <div class="error__close">
-      <i class="bi bi-x-lg close-btn"></i>
-    </div>`;
-  form.insertBefore(errorDiv, submitBtn.nextSibling);
+  </div>`;
 
-  const closeBtn = document.querySelector('.close-btn');
-  closeBtn.addEventListener('click', () => { errorDiv.remove(); });
+  const navbar = document.querySelector('.navbar');
+  navbar.after(errorDiv);
 };
 
 form.addEventListener('submit', (e) => {
@@ -64,8 +35,4 @@ form.addEventListener('submit', (e) => {
     isValid = false;
     showError('Todos los campos son requeridos.');
   } else { form.submit(); }
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  optionMenu.classList.add("active");
 });
