e869bd280f51ddbaad1ea9be27a3f1e6d1301c65
Velastian <cristian13vp@hotmail.com>
2024-12-01 19:13:26 -0600

    Añadido en la pantalla agregar profesores y guia

--- src/public/event/administrador/grupoProfeMaterias.js
+++ src/public/event/administrador/grupoProfeMaterias.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+  //Variables para asignar profesor al grupo
   const grupos = document.getElementById('grupos');
   const profesor = document.getElementById('profesor');
   const materia = document.getElementById('materia');
   const profesor_Guia = document.getElementById('profesor-guia');
   const profeMaterias = document.getElementById('profesorMateria');
-
   var datos_formGrupoProfeMate = {}
 
+  //Variables para asignar profesor guia
+  const grupos_guia = document.getElementById('grupos_guia');
+  const profesor_guia = document.getElementById('profesor_guia');
+  var datos_formGrupoGuia = {};
+
+  //Funcionalidad para asignar profesores al grupo
   mostrarGruposDisponibles('grupos');
   mostrarMateriasDisponibles('materia');
 
@@ -39,7 +45,179 @@ document.addEventListener('DOMContentLoaded', (event) => {
     mostrarProfesor(id_materia, 'profesor');
   });
 
+  var url = '/api/gruposProfeMate_recientes';
+  var tabla_grupoProfeMate = new DataTable('#dt-grupoProfeMate', {
+    processing: true,
+    serverSide: true,
+    deferRender: true,
+    ajax: {
+      url: url,
+      type: 'GET'
+    },
+    aaSorting: [],
+    columns: [
+      { data: "id_detallegrupo" },
+      { data: "Grupo" },
+      { data: "Materia" },
+      { data: "Profesor" },
+      { defaultContent: `<button type="button" class="eliminar btn btn-danger"><i class="fa-solid fa-xmark"></i></button>` }
+    ],
+    columnDefs: [
+      {
+        className: "text-center", targets: [0, 4]
+      },
+      {
+        className: "dt-items-center", targets: [0, 1]
+      }
+    ],
+    destroy: true,
+    responsive: {
+      breakpoints: [
+        { name: 'desktop', width: Infinity },
+        { name: 'tablet-l', width: 1024 },
+        { name: 'tablet-p', width: 768 },
+        { name: 'mobile-l', width: 480 },
+        { name: 'mobile-p', width: 320 }
+      ]
+    },
+    lengthMenu: [5, 10, 15, 20],
+    pageLength: 8,
+    language: {
+      lengthMenu: "Mostrar _MENU_ registros por página",
+      emptyTable: "No hay grupos ingresados...",
+      zeroRecords: "Ningún profesor asignado",
+      info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+      infoEmpty: "Ninguna materia asignada",
+      infoFiltered: "(filtrados desde _MAX_ registros totales)",
+      search: "Buscar:",
+      loadingRecords: "Cargando...",
+      paginate: {
+        first: "Primero",
+        last: "Último",
+        next: "Siguiente",
+        previous: "Anterior"
+      },
+      select: {
+        rows: {
+          _: ' %d Filas seleccionadas',
+          1: ' 1 Fila Seleccionada'
+        }
+      }
+    }
+  });//Datatable para asignar profesor al grupo
 
+  $('#dt-grupoProfeMate tbody').on("click", "button.eliminar", function () {
+    var data_grupoProfeMate = tabla_grupoProfeMate.row($(this).parents("tr")).data();
+    eliminarAsignacion(data_grupoProfeMate);
+  });
+
+
+  //Funcionalidad para asignar profesor guia
+
+  gruposDisponibles('grupos_guia');
+  profesoresDisponibles('profesor_guia');
+
+  $('#btn-asignarGrupo').on('click', function (e) {
+    e.preventDefault();
+    if (validarProfesorGuia() === 0) {
+      datos_formGrupoGuia = { id_grupos: grupos_guia.value, id_profesores: profesor_guia.value };
+      agregarGrupoGuia(datos_formGrupoGuia);
+    }
+
+    const elements = ['#grupos_guia', '#profesor_guia'];
+    elements.forEach(selector => {
+      $(selector).on('change', function () {
+        this.classList.remove('is-invalid');
+        this.classList.replace('border-danger', 'border-secondary');
+      });
+    });//Para limpiar los errores de mi select en agregar profesor guia
+  });
+
+
+  var url2 = '/api/gruposGuia_recientes';
+  var tabla_grupoGuia = new DataTable('#dt-grupoGuia', {
+    processing: true,
+    serverSide: true,
+    deferRender: true,
+    ajax: {
+      url: url2,
+      type: 'GET'
+    },
+    aaSorting: [],
+    columns: [
+      { data: "id_detallegrupo" },
+      { data: "Grupo" },
+      { data: "Profesor" },
+      { defaultContent: `<button type="button" class="eliminar btn btn-danger"><i class="fa-solid fa-xmark"></i></button>` }
+    ],
+    columnDefs: [
+      {
+        className: "text-center", targets: [0, 3,]
+      },
+      {
+        className: "dt-items-center", targets: [0, 1]
+      }
+    ],
+    destroy: true,
+    responsive: {
+      breakpoints: [
+        { name: 'desktop', width: Infinity },
+        { name: 'tablet-l', width: 1024 },
+        { name: 'tablet-p', width: 768 },
+        { name: 'mobile-l', width: 480 },
+        { name: 'mobile-p', width: 320 }
+      ]
+    },
+    lengthMenu: [5, 10, 15, 20],
+    pageLength: 5,
+    language: {
+      lengthMenu: "Mostrar _MENU_ registros por página",
+      emptyTable: "No hay grupos ingresados...",
+      zeroRecords: "Ninguna Grupo encontrado",
+      info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+      infoEmpty: "Ninguna Grupo encontrado",
+      infoFiltered: "(filtrados desde _MAX_ registros totales)",
+      search: "Buscar:",
+      loadingRecords: "Cargando...",
+      paginate: {
+        first: "Primero",
+        last: "Último",
+        next: "Siguiente",
+        previous: "Anterior"
+      },
+      select: {
+        rows: {
+          _: ' %d Filas seleccionadas',
+          1: ' 1 Fila Seleccionada'
+        }
+      }
+    }
+  });//Datatable para asignar profesor guia
+
+  $('#dt-grupoGuia tbody').on("click", "button.eliminar", function () {
+    var data_grupoGuia = tabla_grupoGuia.row($(this).parents("tr")).data();
+    eliminarAsignacionGuia(data_grupoGuia);
+  });
+  
+
+  //Funciones asignar profesor
+  function validarAsignar() {
+    // declaramos las variables
+    const form_grupoGuia = [grupos, profesor, materia];
+    var aux = 0;
+    //Validados que los campos esten correctos
+    Array.from(form_grupoGuia).forEach(select => {
+      if (!select.checkValidity()) {
+        select.classList.add('is-invalid');
+        select.classList.replace('border-secondary', 'border-danger');
+        aux++;
+      } else {
+        select.classList.remove('is-invalid');
+        select.classList.replace('border-danger', 'border-secondary');
+      }
+    });
+    return aux;
+  };//Validar Form
   function agregarGrupoProfeMate(datos_formGrupoProfeMate) {
     axios.post('/api/asignar_gruposProfeMate', datos_formGrupoProfeMate)
       .then(response => {
@@ -72,92 +250,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         .catch(err => console.log('Error', err.message));
     });
   }
-
-  var url = '/api/gruposProfeMate_recientes';
-  var tabla_grupoProfeMate = new DataTable('#dt-grupoProfeMate', {
-    processing: true,
-    serverSide: true,
-    deferRender: true,
-    ajax: {
-      url: url,
-      type: 'GET'
-    },
-    aaSorting: [],
-    columns: [
-      { data: "id_detallegrupo" },
-      { data: "Grupo" },
-      { data: "Materia" },
-      { data: "Profesor" },
-      { defaultContent: `<button type="button" class="eliminar btn btn-danger"><i class="fa-solid fa-trash"></i></button>` }
-    ],
-    columnDefs: [
-      {
-        className: "text-center", targets: [0, 4]
-      },
-      {
-        className: "dt-items-center", targets: [0, 1]
-      }
-    ],
-    destroy: true,
-    responsive: true,
-    responsive: {
-      breakpoints: [
-        { name: 'desktop', width: Infinity },
-        { name: 'tablet-l', width: 1024 },
-        { name: 'tablet-p', width: 768 },
-        { name: 'mobile-l', width: 480 },
-        { name: 'mobile-p', width: 320 }
-      ]
-    },
-    lengthMenu: [5, 10, 15, 20],
-    pageLength: 5,
-    language: {
-      lengthMenu: "Mostrar _MENU_ registros por página",
-      emptyTable: "No hay grupos ingresados...",
-      zeroRecords: "Ninguna materia encontrada",
-      info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
-      infoEmpty: "Ninguna materia asignada",
-      infoFiltered: "(filtrados desde _MAX_ registros totales)",
-      search: "Buscar:",
-      loadingRecords: "Cargando...",
-      paginate: {
-        first: "Primero",
-        last: "Último",
-        next: "Siguiente",
-        previous: "Anterior"
-      },
-      select: {
-        rows: {
-          _: ' %d Filas seleccionadas',
-          1: ' 1 Fila Seleccionada'
-        }
-      }
-    }
-  });
-
-  $('#dt-grupoProfeMate tbody').on("click", "button.eliminar", function () {
-    var data_grupoProfeMate = tabla_grupoProfeMate.row($(this).parents("tr")).data();
-    eliminarAsignacion(data_grupoProfeMate);
-  });
-  //Funciones
-  function validarAsignar() {
-    // declaramos las variables
-    const form_grupoGuia = [grupos, profesor, materia];
-    var aux = 0;
-    //Validados que los campos esten correctos
-    Array.from(form_grupoGuia).forEach(select => {
-      if (!select.checkValidity()) {
-        select.classList.add('is-invalid');
-        select.classList.replace('border-secondary', 'border-danger');
-        aux++;
-      } else {
-        select.classList.remove('is-invalid');
-        select.classList.replace('border-danger', 'border-secondary');
-      }
-    });
-    return aux;
-  };//Validar Form
-
   function limpiar_FormGrupoProfeMate() {
     const elements = ['#profesor'];
     elements.forEach(selector => {
@@ -167,7 +259,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     $('#profesor').prop('disabled', true);
     mostrarMateriasDisponibles('materia');
   };//Limpia los inputs
-
   function mostrarGruposDisponibles(id_select) {
     const grupoView = document.getElementById(id_select);
     axios.get('/api/mostrar_detalleGrupo')
@@ -207,8 +298,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
       })
       .catch(err => console.log('Error', err.message));
   }//Mostramos que las materias disponibles
-
-  function mostrarProfeGuia(id_grupo){
+  function mostrarProfeGuia(id_grupo) {
     axios.post('/api/mostrar_profeGuia', { id_grupo: id_grupo })
       .then(response => {
         const profesor = response.data;
@@ -216,9 +306,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
         mostrarProfeGuiaMateria(profesor[0].id_usuario);
       })
       .catch(err => console.log('Error', err.message));
-      profeMaterias.innerHTML = '';
+    profeMaterias.innerHTML = '';
   }
-  function mostrarProfeGuiaMateria(id_usuario){
+  function mostrarProfeGuiaMateria(id_usuario) {
     axios.post('/api/mostrar_profeGuiaMateria', { id_usuario: id_usuario })
       .then(response => {
         const data = response.data;
@@ -229,8 +319,96 @@ document.addEventListener('DOMContentLoaded', (event) => {
       })
       .catch(err => console.log('Error', err.message));
   }
-  
 
+  //Funciones para asignar profesor guia
+  function validarProfesorGuia() {
+    // declaramos las variables
+    const form_grupoGuia = [grupos_guia, profesor_guia];
+    var aux = 0;
+    //Validados que los campos esten correctos
+    Array.from(form_grupoGuia).forEach(select => {
+      if (!select.checkValidity()) {
+        select.classList.add('is-invalid');
+        select.classList.replace('border-secondary', 'border-danger');
+        aux++;
+      } else {
+        select.classList.remove('is-invalid');
+        select.classList.replace('border-danger', 'border-secondary');
+      }
+    });
+    return aux;
+  };//Validar Form
+  function limpiar_FormGrupoGuia() {
+    const elements = ['#grupos_guia', '#profesor_guia'];
+    elements.forEach(selector => {
+      $(selector).val('');
+    });
+    datos_formGrupoGuia = '';
+    gruposDisponibles('grupos_guia');
+    profesoresDisponibles('profesor_guia');
+  };//Limpia los inputs
+  function agregarGrupoGuia(datos_formGrupoGuia) {
+    axios.post('/api/asignar_gruposGuia', datos_formGrupoGuia)
+      .then(response => {
+        const result = response.data;
+        if (result.success == true) {
+          showToast('success', 'fa-solid fa-circle-check', 'El profesor se asigno con exito!');
+          limpiar_FormGrupoGuia();
+          tabla_grupoGuia.ajax.url(url2).load();
+        } else {
+          showToast('danger', 'bi bi-exclamation-circle-fill', 'Error en el servidor 500!');
+        }
+      })
+      .catch(err => console.log('Error', err.message));
+  }
+  function eliminarAsignacionGuia(data_grupoGuia) {
+    const data = { id_detallegrupo: data_grupoGuia.id_detallegrupo };
+    crearModal('eliminar-grupoGuia', 'btn-aceptar-eliminar-grupo', '¿Deseas quitar este profesor guia?');
+    $("#eliminar-grupoGuia").modal("show");
+    $("#btn-aceptar-eliminar-grupo").on("click", function () {
+      axios.post('/api/eliminar_gruposGuia', data)
+        .then(response => {
+          const result = response.data;
+          if (result.success == true) {
+            showToast('success', 'fa-solid fa-circle-check', 'El profesor se quito con exito!');
+            limpiar_FormGrupoGuia();
+            tabla_grupoGuia.ajax.url(url2).load();
+          } else {
+            showToast('danger', 'bi bi-exclamation-circle-fill', 'Error en el servidor 500!');
+          }
+        })
+        .catch(err => console.log('Error', err.message));
+    });
+  }
+  function gruposDisponibles(id_select) {
+    const grupoView = document.getElementById(id_select);
+    axios.get('/api/mostrar_gruposGuia')
+      .then(response => {
+        const grupo = response.data;
+        grupoView.innerHTML = '<option selected disabled value="">Elegir...</option>';
+        for (let i = 0; i < grupo.length; i++) {
+          grupoView.innerHTML += `
+                <option value=${grupo[i].id_detallegrupo}>${grupo[i].Grupo}</option>`;
+        };
+      })
+      .catch(err => console.log('Error', err.message));
+  }//Mostramos que los grupos disponibles sin asignar
+  function profesoresDisponibles(id_select) {
+    const profesorView = document.getElementById(id_select);
+    axios.get('/api/mostrar_profesorGuia')
+      .then(response => {
+        const profesor = response.data;
+        profesorView.innerHTML = '<option selected disabled value="">Elegir...</option>';
+        for (let i = 0; i < profesor.length; i++) {
+          profesorView.innerHTML += `
+                <option value=${profesor[i].id_usuario}>${profesor[i].Profesor}</option>`;
+        };
+      })
+      .catch(err => console.log('Error', err.message));
+  }//Mostramos que los grupos disponibles sin asignar
+
+
+  //Componentes
   function showToast(tipo, icono, mensaje) {
     const messageDiv = document.getElementById('toast-notificacion');
     messageDiv.innerHTML = '';
